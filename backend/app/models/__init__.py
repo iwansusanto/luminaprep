f544d2fbbaac84f28d810b7ce6fb4be47cefbd05
iwan susanto <iwandevapps@gmail.com>
2026-05-13 18:15:42 +0700

    fix login issue

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,19 @@
 from .user import User, UserCreate, UserRead, UserLogin
 from .project import Project, ProjectCreate, ProjectRead, ProjectUpdate
+from .material import Material, MaterialCreate, MaterialRead, MaterialUpdate
+from .quiz import Quiz, QuizCreate, QuizRead, QuizUpdate
+from .question import Question
+from .user_attempt import UserAttempt
+from .quiz_session import QuizSession
+from .agent_metric import AgentMetric
 
 __all__ = [
     "User", "UserCreate", "UserRead", "UserLogin",
-    "Project", "ProjectCreate", "ProjectRead", "ProjectUpdate"
+    "Project", "ProjectCreate", "ProjectRead", "ProjectUpdate",
+    "Material", "MaterialCreate", "MaterialRead", "MaterialUpdate",
+    "Quiz", "QuizCreate", "QuizRead", "QuizUpdate",
+    "Question",
+    "UserAttempt",
+    "QuizSession",
+    "AgentMetric",
 ]
